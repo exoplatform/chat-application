f36dba47f2986af567855ee20a28ba2e470739db
Benjamin Paillereau <bpaillereau@exoplatform.com>
2012-11-15 13:57:11 +0100

    multi server copy

--- copie.sh
+++ copie.sh
@@ -1,5 +1,12 @@
-cp server/target/chatServer.jar ~/java/demos/eXo-Platform-tomcat-3.5.4/tomcat-bundle/lib/
-rm -Rf ~/java/demos/eXo-Platform-tomcat-3.5.4/tomcat-bundle/webapps/chatServer/
-cp server/target/chatServer.war ~/java/demos/eXo-Platform-tomcat-3.5.4/tomcat-bundle/webapps/
+#/Users/benjamin/java/demos/apache-tomcat-7.0.30
+
+### CLIENT
 rm -Rf ~/java/demos/eXo-Platform-tomcat-3.5.4/tomcat-bundle/webapps/chat/
+cp server/target/chatServer.jar ~/java/demos/eXo-Platform-tomcat-3.5.4/tomcat-bundle/lib/
 cp application/target/chat.war ~/java/demos/eXo-Platform-tomcat-3.5.4/tomcat-bundle/webapps/
+
+
+### SERVER
+rm -Rf ~/java/demos/eXo-Platform-tomcat-3.5.4/tomcat-bundle/webapps/chatServer/
+cp server/target/chatServer.war ~/java/demos/apache-tomcat-7.0.30/webapps/
+
