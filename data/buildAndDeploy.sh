9d72931d90b62f132d83855b3649e19dd6698e6b
Benjamin Paillereau <bpaillereau@exoplatform.com>
2012-11-23 08:33:12 +0100

    moving stuff to data folder

--- data/buildAndDeploy.sh
+++ data/buildAndDeploy.sh
@@ -13,14 +13,14 @@ cd /Users/benjamin/git/chat
 mvn clean install -Dmaven.test.skip=true -pl server/
 rm -Rf ~/java/demos/apache-tomcat-7.0.30/webapps/chatServer.war
 rm -Rf ~/java/demos/apache-tomcat-7.0.30/webapps/chatServer/
-cp "$properties" ~/java/demos/apache-tomcat-7.0.30/conf/chat.properties
+cp "data/$properties" ~/java/demos/apache-tomcat-7.0.30/conf/chat.properties
 cp server/target/chatServer.war ~/java/demos/apache-tomcat-7.0.30/webapps/
 
 ### CLIENT
 mvn clean install -Dmaven.test.skip=true -pl application/
 rm -Rf ~/java/demos/eXo-Platform-tomcat-3.5.4/tomcat-bundle/webapps/chat.war
 rm -Rf ~/java/demos/eXo-Platform-tomcat-3.5.4/tomcat-bundle/webapps/chat/
-cp "$properties" ~/java/demos/eXo-Platform-tomcat-3.5.4/tomcat-bundle/conf/chat.properties
+cp "data/$properties" ~/java/demos/eXo-Platform-tomcat-3.5.4/tomcat-bundle/conf/chat.properties
 cp server/target/chatServer.jar ~/java/demos/eXo-Platform-tomcat-3.5.4/tomcat-bundle/lib/
 cp application/target/chat.war ~/java/demos/eXo-Platform-tomcat-3.5.4/tomcat-bundle/webapps/
 
