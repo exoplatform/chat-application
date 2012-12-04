5860216dd5501b7f9f8f67191752bce0ab7b8978
Benjamin Paillereau <bpaillereau@exoplatform.com>
2012-12-04 17:55:47 +0100

    modified buildAndDeploy

--- data/buildAndDeploy.sh
+++ data/buildAndDeploy.sh
@@ -1,28 +1,32 @@
 #!/bin/bash
 
+SCRIPT_LAUNCH_DIR=$(pwd)
+PROJECT_DIR=$(cd $(dirname "$0"); pwd)
+#PLF_TOMCAT_DIRECTORY=/Users/benjamin/java/demos/eXo-Platform-tomcat-3.5.4/tomcat-bundle
+#CHT_TOMCAT_DIRECTORY=/Users/benjamin/java/demos/apache-tomcat-7.0.30
+PLF_TOMCAT_DIRECTORY=/Users/benjamin/java/demos/eXo-Platform-tomcat-3.5.5/tomcat-bundle
+CHT_TOMCAT_DIRECTORY=/Users/benjamin/java/demos/eXo-Platform-tomcat-3.5.5/tomcat-bundle
+
 if [ $# = 1 ]
 then
-  properties=$1
+  PROPERTIES=$1
 else
-  properties="chat.properties"
+  PROPERTIES="chat.properties"
 fi
 
 cd /Users/benjamin/git/chat
 
 ### SERVER
 mvn clean install -Dmaven.test.skip=true -pl server/
-rm -Rf ~/java/demos/apache-tomcat-7.0.30/webapps/chatServer.war
-rm -Rf ~/java/demos/apache-tomcat-7.0.30/webapps/chatServer/
-cp "data/$properties" ~/java/demos/apache-tomcat-7.0.30/conf/chat.properties
-cp server/target/chatServer.war ~/java/demos/apache-tomcat-7.0.30/webapps/
+rm -Rf $CHT_TOMCAT_DIRECTORY/webapps/chatServer.war
+rm -Rf $CHT_TOMCAT_DIRECTORY/webapps/chatServer/
+cp "data/$PROPERTIES" $CHT_TOMCAT_DIRECTORY/conf/chat.properties
+cp server/target/chatServer.war $CHT_TOMCAT_DIRECTORY/webapps/
 
 ### CLIENT
 mvn clean install -Dmaven.test.skip=true -pl application/
-rm -Rf ~/java/demos/eXo-Platform-tomcat-3.5.4/tomcat-bundle/webapps/chat.war
-rm -Rf ~/java/demos/eXo-Platform-tomcat-3.5.4/tomcat-bundle/webapps/chat/
-cp "data/$properties" ~/java/demos/eXo-Platform-tomcat-3.5.4/tomcat-bundle/conf/chat.properties
-cp server/target/chatServer.jar ~/java/demos/eXo-Platform-tomcat-3.5.4/tomcat-bundle/lib/
-cp application/target/chat.war ~/java/demos/eXo-Platform-tomcat-3.5.4/tomcat-bundle/webapps/
-
-
-
+rm -Rf $PLF_TOMCAT_DIRECTORY/webapps/chat.war
+rm -Rf $PLF_TOMCAT_DIRECTORY/webapps/chat/
+cp "data/$PROPERTIES" $PLF_TOMCAT_DIRECTORY/conf/chat.properties
+cp server/target/chatServer.jar $PLF_TOMCAT_DIRECTORY/lib/
+cp application/target/chat.war $PLF_TOMCAT_DIRECTORY/webapps/
