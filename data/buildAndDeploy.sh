3ac7cea233c7cfe77c3e441e3e5f88696671957b
Benjamin Paillereau <bpaillereau@gmail.com>
2013-02-13 12:50:22 +0100

    auto deploy sharedLayout in platform (not a best practice but whatever)

--- data/buildAndDeploy.sh
+++ data/buildAndDeploy.sh
@@ -2,10 +2,12 @@
 
 SCRIPT_LAUNCH_DIR=$(pwd)
 PROJECT_DIR=$(cd $(dirname "$0"); pwd)
+
 CHT_TOMCAT_DIRECTORY=/Users/benjamin/servers/apache-tomcat-7.0.35
-PLF_TOMCAT_DIRECTORY=/Users/benjamin/servers/eXo-Platform-tomcat-3.5.6-BEN/tomcat-bundle
-#PLF_TOMCAT_DIRECTORY=/Users/benjamin/servers/eXo-Platform-tomcat-3.5.6/tomcat-bundle
 #CHT_TOMCAT_DIRECTORY=/Users/benjamin/servers/eXo-Platform-tomcat-3.5.6/tomcat-bundle
+
+PLF_TOMCAT_DIRECTORY=/Users/benjamin/servers/eXo-Platform-tomcat-3.5.6/tomcat-bundle
+#PLF_TOMCAT_DIRECTORY=/Users/benjamin/servers/eXo-Platform-tomcat-3.5.6-BEN/tomcat-bundle
 
 if [ $# = 1 ]
 then
@@ -13,6 +15,7 @@ then
 else
   PROPERTIES="data/chat.properties"
 fi
+SHARED_LAYOUT="data/sharedLayout.xml"
 
 cd $PROJECT_DIR/..
 
@@ -28,5 +31,6 @@ mvn clean install -Dmaven.test.skip=true -pl application/
 rm -Rf $PLF_TOMCAT_DIRECTORY/webapps/chat.war
 rm -Rf $PLF_TOMCAT_DIRECTORY/webapps/chat/
 cp "$PROPERTIES" $PLF_TOMCAT_DIRECTORY/conf/chat.properties
+cp "$SHARED_LAYOUT" $PLF_TOMCAT_DIRECTORY/webapps/platform-extension/WEB-INF/conf/portal/portal/
 cp server/target/chatServer.jar $PLF_TOMCAT_DIRECTORY/lib/
 cp application/target/chat.war $PLF_TOMCAT_DIRECTORY/webapps/
