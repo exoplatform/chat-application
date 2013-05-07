52a4fb761b844db129bd89afe56ea12aa181876b
Benjamin Paillereau <bpaillereau@gmail.com>
2013-05-07 15:35:39 +0200

    upgrade to juzu 0.6.0

--- data/buildAndDeploy.sh
+++ data/buildAndDeploy.sh
@@ -3,11 +3,11 @@
 SCRIPT_LAUNCH_DIR=$(pwd)
 PROJECT_DIR=$(cd $(dirname "$0"); pwd)
 
-CHT_TOMCAT_DIRECTORY=/Users/benjamin/servers/platform-4.0.0-RC1
+CHT_TOMCAT_DIRECTORY=/Users/benjamin/servers/platform-4.0.0
 #CHT_TOMCAT_DIRECTORY=/Users/benjamin/servers/apache-tomcat-7.0.35
 #CHT_TOMCAT_DIRECTORY=/Users/benjamin/servers/eXo-Platform-tomcat-3.5.6/tomcat-bundle
 
-PLF_TOMCAT_DIRECTORY=/Users/benjamin/servers/platform-4.0.0-RC1
+PLF_TOMCAT_DIRECTORY=/Users/benjamin/servers/platform-4.0.0
 #PLF_TOMCAT_DIRECTORY=/Users/benjamin/servers/eXo-Platform-tomcat-3.5.6/tomcat-bundle
 #PLF_TOMCAT_DIRECTORY=/Users/benjamin/servers/eXo-Platform-tomcat-3.5.6-BEN/tomcat-bundle
 
