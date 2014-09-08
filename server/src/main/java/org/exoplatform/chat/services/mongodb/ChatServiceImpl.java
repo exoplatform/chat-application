521e00c0c3b9c2e5bf9ed45565a1b0a71ebcb207
Pham Duy Dong <dongpd@exoplatform.com>
2014-09-08 15:19:24 +0700

    CHAT-55 Chat Add-on: Problem when creating event and task with special characters

--- server/src/main/java/org/exoplatform/chat/services/mongodb/ChatServiceImpl.java
+++ server/src/main/java/org/exoplatform/chat/services/mongodb/ChatServiceImpl.java
@@ -104,11 +104,6 @@ public class ChatServiceImpl implements org.exoplatform.chat.services.ChatServic
     {
       options = options.replaceAll("<", "&lt;");
       options = options.replaceAll(">", "&gt;");
-      if (options.contains("type-event")) {
-        options = options.replaceAll("'", "\\\\\"");
-      }
-//      options = options.replaceAll("\"", "&quot;");
-//      options = options.replaceAll("\\\\", "&#92");
       doc.put("options", options);
     }
     coll.insert(doc);
