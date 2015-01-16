61b26d67ec78f2d716600d14acdbefdc94007fa3
Mohamed Ali Dhifallah <madhifallah@exoplatform.com>
2015-01-16 18:37:18 +0100

    get the configuration file from .server.config.dir/gatein/chat.properties if the AS is JBOSS

--- exo-addons-chat-extension-config/src/main/java/org/exoplatform/addons/chat/utils/PropertyManager.java
+++ exo-addons-chat-extension-config/src/main/java/org/exoplatform/addons/chat/utils/PropertyManager.java
@@ -29,9 +29,18 @@ public class PropertyManager {
 
   private static Properties properties;
 
-  private static final String PROPERTIES_PATH = System.getProperty("catalina.base")+"/conf/chat.properties";
+    private static final String PROPERTIES_PATH;
 
-  public static final String PROPERTY_SYSTEM_PREFIX = "chat.";
+    static {
+        if ((System.getProperty("catalina.base") == null) || System.getProperty("catalina.base").isEmpty()) {
+            PROPERTIES_PATH =  System.getProperty("jboss.server.config.dir") + "/gatein/chat.properties";
+        }    else {
+
+            PROPERTIES_PATH = System.getProperty("catalina.base") + "/conf/chat.properties";
+        }
+    }
+
+    public static final String PROPERTY_SYSTEM_PREFIX = "chat.";
   public static final String PROPERTY_PASSPHRASE = "chatPassPhrase";
 
   public static String getProperty(String key)
