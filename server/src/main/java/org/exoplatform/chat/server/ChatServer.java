56c2c3850b454e0655bb21174864cde3fae2dcfd
nguyenthienhuong <nguyenthienhuong0305@gmail.com>
2015-02-04 16:58:33 +0700

    CHAT-144: Display incorrect when team's name contains special characters
    Fix description: Remove escapeHtml4 before put to json list for viewing.

--- server/src/main/java/org/exoplatform/chat/server/ChatServer.java
+++ server/src/main/java/org/exoplatform/chat/server/ChatServer.java
@@ -576,7 +576,7 @@ public class ChatServer
 
       }
 
-      jsonObject.put("name", StringEscapeUtils.escapeHtml4(teamName));
+      jsonObject.put("name", teamName);
       jsonObject.put("room", room);
 
     }
