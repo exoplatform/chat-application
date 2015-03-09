2043c29ae08e357c800c6780144f97029e8efc75
Romain Dénarié <romain.denarie@exoplatform.com>
2015-03-09 13:28:31 +0100

    reorganize import

--- server/src/main/java/org/exoplatform/chat/server/ChatServer.java
+++ server/src/main/java/org/exoplatform/chat/server/ChatServer.java
@@ -57,7 +57,13 @@ import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.GregorianCalendar;
+import java.util.List;
+import java.util.Locale;
+import java.util.Arrays;
+import java.util.Properties;
+
 import java.util.logging.Logger;
 
 @ApplicationScoped
