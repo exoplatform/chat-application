e7dd4a6344f481b7b7d92d4e2a7b9a2d3b4dea75
vannd <vannd@exoplatform.com>
2015-11-20 15:10:33 +0700

    CHAT-336: Compilation error due to API changes in WikiService
    Fix description: update new Wiki API

--- application/src/main/java/org/exoplatform/chat/portlet/chat/WikiService.java
+++ application/src/main/java/org/exoplatform/chat/portlet/chat/WikiService.java
@@ -7,6 +7,7 @@ import org.exoplatform.wiki.mow.api.PermissionEntry;
 import org.exoplatform.wiki.mow.api.PermissionType;
 import org.exoplatform.wiki.mow.api.Permission;
 import org.exoplatform.wiki.resolver.TitleResolver;
+import org.exoplatform.wiki.service.IDType;
 import org.xwiki.rendering.syntax.Syntax;
 
 import javax.enterprise.context.ApplicationScoped;
@@ -34,9 +35,9 @@ public class WikiService {
     wikiService_ = wikiService;
   }
 
-  protected String createIntranetPage(String title, String content, ArrayList<String> users)
+  protected String createIntranetPage(String creator, String title, String content, ArrayList<String> users)
   {
-    return createOrEditPage("Meeting Notes", title, content, users, false, null);
+    return createOrEditPage(creator, "Meeting Notes", title, content, users, false, null);
   }
 
   /**
@@ -45,12 +46,12 @@ public class WikiService {
    * @param content
    * @param spaceGroupId : format with spaces/space_group_name
    */
-  protected String createSpacePage(String title, String content, String spaceGroupId, ArrayList<String> users)
+  protected String createSpacePage(String creator, String title, String content, String spaceGroupId, ArrayList<String> users)
   {
-    return createOrEditPage("Meeting Notes", title, content, users, false, spaceGroupId);
+    return createOrEditPage(creator, "Meeting Notes", title, content, users, false, spaceGroupId);
   }
 
-  private String createOrEditPage(String parentTitle, String title, String content, ArrayList<String> users, boolean forceNew, String spaceGroupId)
+  private String createOrEditPage(String creator, String parentTitle, String title, String content, ArrayList<String> users, boolean forceNew, String spaceGroupId)
   {
     String wikiType = PortalConfig.PORTAL_TYPE;
     String wikiOwner = "intranet";
@@ -69,49 +70,48 @@ public class WikiService {
         if (!wikiService_.isExisting(wikiType, wikiOwner, TitleResolver.getId(parentTitle, false))) {
           Page ppage = new Page();
           ppage.setTitle(parentTitle);
+          ppage.setContent("= " + parentTitle + " =\n");
+          ppage.setSyntax(Syntax.XWIKI_2_0.toIdString());
+          ppage.setOwner(creator);
+          ppage.setAuthor(creator);
           Wiki pwiki = new Wiki();
           pwiki.setOwner(wikiOwner);
           pwiki.setType(wikiType);
-          Page createdPpage = wikiService_.createPage(pwiki, "WikiHome", ppage);
-          createdPpage.setContent("= " + parentTitle + " =\n");
-          createdPpage.setSyntax(Syntax.XWIKI_2_0.toIdString());
-          wikiService_.createVersionOfPage(createdPpage);
+          wikiService_.createPage(pwiki, "WikiHome", ppage);
         }
 
         Page page = new Page();
         page.setTitle(title);
+        page.setContent(content);
+        page.setSyntax(Syntax.XWIKI_2_0.toIdString());
+        setPermissionForReportAsWiki(users, page);
+        page.setOwner(creator);
+        page.setAuthor(creator);
+        page.setMinorEdit(false);
+        if (wikiType.equals(PortalConfig.GROUP_TYPE)) {
+            // http://demo.exoplatform.net/portal/intranet/wiki/group/spaces/bank_project/Meeting_06-11-2013
+            path = "/portal/intranet/wiki/" + wikiType + wikiOwner + "/" + TitleResolver.getId(title, false);
+          } else if (wikiType.equals(PortalConfig.PORTAL_TYPE)) {
+            // http://demo.exoplatform.net/portal/intranet/wiki/Sales_Meetings_Meeting_06-11-2013
+            path = "/portal/intranet/wiki/" + TitleResolver.getId(title, false);
+          }
+        page.setUrl(path);
         Wiki wiki = new Wiki();
         wiki.setOwner(wikiOwner);
         wiki.setType(wikiType);
-        Page createdPage;
         boolean isPageExisted = false;
         if (wikiService_.isExisting(wikiType, wikiOwner, TitleResolver.getId(title, false))) {
-          createdPage = wikiService_.getPageById(TitleResolver.getId(title, false));
+          wikiService_.getPageById(TitleResolver.getId(title, false));
           isPageExisted = true;
         } else {
           try {
-            createdPage =  wikiService_.createPage(wiki, parentTitle, page);
+            wikiService_.createPage(wiki, TitleResolver.getId(parentTitle, false), page);
           } catch (Exception e) {
             isPageExisted = true;
-            createdPage = wikiService_.getPageById(TitleResolver.getId(title, false));
+            wikiService_.getPageById(TitleResolver.getId(title, false));
           }
         }
-
-        createdPage.setContent(content);
-        setPermissionForReportAsWiki(users, createdPage);
-        createdPage.setSyntax(Syntax.XWIKI_2_0.toIdString());
-        createdPage.setMinorEdit(false);
-        wikiService_.createVersionOfPage(createdPage);
-
-        if (wikiType.equals(PortalConfig.GROUP_TYPE)) {
-          // http://demo.exoplatform.net/portal/intranet/wiki/group/spaces/bank_project/Meeting_06-11-2013
-          path = "/portal/intranet/wiki/" + wikiType + wikiOwner + "/" + createdPage.getName();
-        } else if (wikiType.equals(PortalConfig.PORTAL_TYPE)) {
-          // http://demo.exoplatform.net/portal/intranet/wiki/Sales_Meetings_Meeting_06-11-2013
-          path = "/portal/intranet/wiki/" + createdPage.getName();
-        }
-        createdPage.setUrl(path);
-       }
+      }
     } catch (Exception e) {
       LOG.log(Level.SEVERE, "Unknown exception", e);
     }
@@ -124,17 +124,10 @@ public class WikiService {
               new Permission(PermissionType.VIEWPAGE, true),
               new Permission(PermissionType.EDITPAGE, true),
       };
-      List<PermissionEntry> permissions = page.getPermissions();
-      int anyIndex = -1;
-      for (int i = 0; i < permissions.size(); i ++) {
-        PermissionEntry any = permissions.get(i);
-        if (ANY.equals(any.getFullName())) anyIndex = i;
-      }
-      if (anyIndex > -1 ) permissions.remove(anyIndex);
+      List<PermissionEntry> permissions = new ArrayList<PermissionEntry>();
       for (int i = 0; i < users.size(); i++) {
-        PermissionEntry userPermission = new PermissionEntry();
-        userPermission.setFullName(users.get(i).toString());
-        userPermission.setPermissions(allPermissions);
+        String strUser = users.get(i).toString();
+        PermissionEntry userPermission = new PermissionEntry(strUser, strUser, IDType.USER, allPermissions);
         permissions.add(userPermission);
       }
       page.setPermissions(permissions);
