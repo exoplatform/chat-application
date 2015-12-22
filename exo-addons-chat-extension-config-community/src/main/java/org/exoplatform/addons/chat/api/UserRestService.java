dc415f03a1c8dfa1b20394f5e3de9826a84d60db
Pham Duy Dong <dongpd@exoplatform.com>
2015-12-22 17:49:08 +0700

    BITNAMI-43 Cannot access user avatar (chat, video call) with HTTPS
    Analysis: - Https Rest calling an inner https Rest needs a correct ssl certificate or a dummy cert with correct common name.
              - This makes geting avartar Rest can not work properly with https when using bitnami built-in dummy ssl cert.
    Solution: - Call jcr directly instead of rest to get social avarta

--- exo-addons-chat-extension-config-community/src/main/java/org/exoplatform/addons/chat/api/UserRestService.java
+++ exo-addons-chat-extension-config-community/src/main/java/org/exoplatform/addons/chat/api/UserRestService.java
@@ -3,10 +3,14 @@ package org.exoplatform.addons.chat.api;
 
 import org.exoplatform.addons.chat.utils.MessageDigester;
 import org.exoplatform.addons.chat.utils.PropertyManager;
+import org.exoplatform.services.cms.link.NodeFinder;
 import org.exoplatform.services.rest.resource.ResourceContainer;
 import org.exoplatform.services.security.ConversationState;
+import org.exoplatform.services.wcm.utils.WCMCoreUtils;
 
 import javax.annotation.security.RolesAllowed;
+import javax.jcr.Node;
+import javax.servlet.ServletContext;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
@@ -18,72 +22,24 @@ import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 import javax.ws.rs.core.UriInfo;
 import java.io.InputStream;
-import java.net.URL;
-import java.net.URLConnection;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 @Path("/chat/api/1.0/user/")
-public class UserRestService implements ResourceContainer
-{
-  /** The Constant LAST_MODIFIED_PROPERTY. */
-  protected static final String LAST_MODIFIED_PROPERTY = "Last-Modified";
-
-  /** The Constant IF_MODIFIED_SINCE_DATE_FORMAT. */
-  protected static final String IF_MODIFIED_SINCE_DATE_FORMAT = "EEE, dd MMM yyyy HH:mm:ss z";
+public class UserRestService implements ResourceContainer {
 
   public static final String ANONIM_USER = "__anonim_";
 
+  /* The Constant LAST_MODIFIED_PROPERTY */
+  protected static final String LAST_MODIFIED_PROPERTY = "Last-Modified";
 
-  public UserRestService()
-  {
-  }
-
-  @GET
-  @Path("/getAvatarURL/{userId}/")
-  @RolesAllowed("users")
-  public Response getAvatarURL(@PathParam("userId") String userId, @Context UriInfo uri) {
-
-    CacheControl cacheControl = new CacheControl();
-    DateFormat dateFormat = new SimpleDateFormat(IF_MODIFIED_SINCE_DATE_FORMAT);
-
-    // Get server base
-    String scheme = uri.getBaseUri().getScheme();
-    String serverName = uri.getBaseUri().getHost();
-    int serverPort = uri.getBaseUri().getPort();
-    String serverBase = scheme + "://" + serverName;
-    if (serverPort != 80) serverBase += ":" + serverPort;
-
-    // Get avatar
-    InputStream in = null;
-    try {
-      URL url = new URL(serverBase +
-              "/rest/jcr/repository/social/production/soc:providers/soc:organization/soc:" + userId +
-              "/soc:profile/soc:avatar");
-      URLConnection con = url.openConnection();
-      con.setDoOutput(true);
-      in = con.getInputStream();
-    } catch (Exception e) {
-      try {
-        URL url = new URL(serverBase + "/eXoSkin/skin/images/themes/default/social/skin/ShareImages/UserAvtDefault.png");
-        URLConnection con = url.openConnection();
-        con.setDoOutput(true);
-        in = con.getInputStream();
-      } catch (Exception e1) {
-        return Response.status(Status.NOT_FOUND).build();
-      }
-    }
-
-    return Response.ok(in, "Image").cacheControl(cacheControl)
-            .header(LAST_MODIFIED_PROPERTY, dateFormat.format(new Date()))
-            .build();
-  }
+  /* The Constant IF_MODIFIED_SINCE_DATE_FORMAT */
+  protected static final String IF_MODIFIED_SINCE_DATE_FORMAT = "EEE, dd MMM yyyy HH:mm:ss z";
 
   @GET
   @Path("/token/")
-  public Response getToken(@QueryParam("tokenOnly") String tokenOnly) throws Exception
-  {
+  public Response getToken(@QueryParam("tokenOnly") String tokenOnly) throws Exception {
     ConversationState conversationState = ConversationState.getCurrent();
     String userId = conversationState.getIdentity().getUserId();
     String token;
@@ -92,14 +48,13 @@ public class UserRestService implements ResourceContainer
     cacheControl.setNoStore(true);
     DateFormat dateFormat = new SimpleDateFormat(IF_MODIFIED_SINCE_DATE_FORMAT);
 
-    boolean withTokenOnly = (tokenOnly!=null && "true".equals(tokenOnly));
-    if ("__anonim".equals(userId))
-    {
+    boolean withTokenOnly = (tokenOnly != null && "true".equals(tokenOnly));
+    if ("__anonim".equals(userId)) {
       userId = ANONIM_USER;
       token = "---";
     } else {
       String passphrase = PropertyManager.getProperty(PropertyManager.PROPERTY_PASSPHRASE);
-      String in = userId+passphrase;
+      String in = userId + passphrase;
       token = MessageDigester.getHash(in);
     }
 
@@ -116,6 +71,49 @@ public class UserRestService implements ResourceContainer
 
     return Response.ok(sb.toString(), MediaType.APPLICATION_JSON)
             .cacheControl(cacheControl)
+            .header(LAST_MODIFIED_PROPERTY, dateFormat.format(new Date()))
+            .build();
+  }
+
+  @GET
+  @Path("/getAvatarURL/{userId}/")
+  @RolesAllowed("users")
+  public Response getAvatarURL(@PathParam("userId") String userId) {
+    return getAvartar(false, userId);
+  }
+
+  @GET
+  @Path("/getSpaceAvartar/{spaceName}/")
+  @RolesAllowed("users")
+  public Response getSpaceAvartar(@PathParam("spaceName") String spaceName) {
+    return getAvartar(true, spaceName);
+  }
+
+  private Response getAvartar(boolean isSpace, String spaceOrUserId) {
+    CacheControl cacheControl = new CacheControl();
+    DateFormat dateFormat = new SimpleDateFormat(IF_MODIFIED_SINCE_DATE_FORMAT);
+
+    // Get avatar
+    StringBuilder avartaNodePathBuilder = new StringBuilder();
+    avartaNodePathBuilder.append("/production/soc:providers/soc:");
+    avartaNodePathBuilder.append(isSpace ? "space" : "organization");
+    avartaNodePathBuilder.append("/soc:").append(spaceOrUserId).append("/soc:profile/soc:avatar");
+    InputStream avartaInputStream = null;
+    try {
+      Node avartaNode = (Node) WCMCoreUtils.getService(NodeFinder.class).getItem("social", avartaNodePathBuilder
+              .toString(), true);
+      avartaInputStream = avartaNode.getNode("jcr:content").getProperty("jcr:data").getStream();
+    } catch (Exception e) {
+      try {
+        String defaultAvartarURL = "img/UserAvtDefault.png";
+        if (isSpace) defaultAvartarURL = "img/SpaceChatAvatar.png";
+        avartaInputStream = getClass().getClassLoader().getResourceAsStream(defaultAvartarURL);
+      } catch (Exception e1) {
+        return Response.status(Status.NOT_FOUND).build();
+      }
+    }
+
+    return Response.ok(avartaInputStream, "Image").cacheControl(cacheControl)
             .header(LAST_MODIFIED_PROPERTY, dateFormat.format(new Date()))
             .build();
   }
