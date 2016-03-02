6701633ff8aff832956f57a748d49dd1b4bf1c7e
smartytwiti <marwen109@gmail.com>
2016-03-02 17:26:06 +0100

    added support for ctr+enter alt+enter

--- application/src/main/webapp/js/chat.js
+++ application/src/main/webapp/js/chat.js
@@ -122,11 +122,11 @@ var chatApplication = new ChatApplication();
     $('#msg').keydown(function(event) {
       //prevent the default behavior of the enter button
       if ( event.which == 13 ) {
-          event.preventDefault();
+         event.preventDefault();
         }
-       //adding shift+enter for adding carriage return in a specific cursor
-      if (event.keyCode == 13 && event.shiftKey) {
-          this.value = this.value.substring(0, this.selectionStart)+"\n"+this.value.substring(this.selectionEnd,this.value.length);
+      //adding (shift or ctl or alt) + enter for adding carriage return in a specific cursor
+      if ( event.keyCode == 13 && (event.shiftKey||event.ctrlKey||event.altKey) ) {
+         this.value = this.value.substring(0, this.selectionStart)+"\n"+this.value.substring(this.selectionEnd,this.value.length);
         }
   //    console.log("keydown : "+ event.which+" ; "+keydown);
       if ( event.which == 18 ) {
@@ -137,11 +137,10 @@ var chatApplication = new ChatApplication();
     $('#msg').keyup(function(event) {
       var msg = $(this).val();
   //    console.log("keyup : "+event.which + ";"+msg.length+";"+keydown);
-      if ( event.which === 13 && keydown !== 18 && msg.trim().length>=1) {
+      if ( event.which === 13 && msg.trim().length>=1) {
         //console.log("sendMsg=>"+username + " : " + room + " : "+msg);
-        if(!msg)
-        {
-          return;
+        if ( !msg || event.keyCode == 13 && (event.shiftKey||event.ctrlKey||event.altKey) ) {
+          return false;
         }
   //      console.log("*"+msg+"*");
         chatApplication.sendMessage(msg);
