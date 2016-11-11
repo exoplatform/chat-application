7a1a4f88fab63e6a71ba00dc3139f448a037f738
LamiaBouhajja <lbouhajja@exoplatform.com>
2016-11-11 18:21:20 +0100

    CHAT-495 Make Chat contact panel responsive : Description : - Force scroll bottom when open keyboar -- To be tested on real devices -- - Remove email from nav bar menu. - Functional chat room users status. - Correct submit msg btn feature after chat updates.

--- application/src/main/webapp/js/chat.js
+++ application/src/main/webapp/js/chat.js
@@ -66,7 +66,7 @@ var chatApplication = new ChatApplication();
      *
      */
     //needed for #chat text area
-    var keydown = -1;
+    chatApplication.keydown = -1;
     //needed for #edit-modal-text area
     var keydownModal = -1;
     //needed for Fluid Integration
@@ -141,7 +141,7 @@ var chatApplication = new ChatApplication();
       }
   //    console.log("keydown : "+ event.which+" ; "+keydown);
       if ( event.which == 18 ) {
-        keydown = 18;
+        chatApplication.keydown = 18;
       }
     });
 
@@ -166,11 +166,11 @@ var chatApplication = new ChatApplication();
         }
       }
 
-      if ( keydown === 18 ) {
-        keydown = -1;
+      if ( chatApplication.keydown === 18 ) {
+        chatApplication.keydown = -1;
       }
       if ( event.which === 13 ) {
-        document.getElementById("msg").value = '';
+        $(this).val('');
       }
 
     });
@@ -184,28 +184,10 @@ var chatApplication = new ChatApplication();
 
     $("#submit").on("click", function() {
 
-        var msg = $("#msg").val();
-
-        //    console.log("keyup : "+event.which + ";"+msg.length+";"+keydown);
-          if ( msg.trim().length>=1) {
+            var msg = $("#msg_editable").text();
             chatApplication.sendMessage(msg);
-
-          }
-          // UP Arrow
-          if (event.which === 38 && msg.length === 0) {
-            var $uimsg = chatApplication.chatRoom.getUserLastMessage();
-            var $uimsgdata = $uimsg.find(".msg-data");
-            if ($uimsgdata.length === 1) {
-              chatApplication.openEditMessagePopup($uimsgdata.attr("data-id"), $uimsgdata.html());
-            }
-          }
-
-          if ( keydown === 18 ) {
-            keydown = -1;
-          }
-          if ( event.which === 13 ) {
-            document.getElementById("msg").value = '';
-          }
+            $("#msg_editable").text("");
+            $("#msg, #msg_editable").focus();
 
     });
 
@@ -415,6 +397,9 @@ var chatApplication = new ChatApplication();
             }, 500);
 
             jqchat("#chats").css("min-height", "0");
+
+            jqchat("#chat-room-detail-avatar, .chat-message.footer, #searchButton2, #chat-video-button").css("display", "block");
+            jqchat("#userRoomStatus").removeClass("hide");
         }
         chatApplication.loadRoom();
     });
@@ -480,11 +465,11 @@ var chatApplication = new ChatApplication();
     });
 
     $(document).on("keyup", "input[name='keyWords']", function(event) {//when entering keywords save them imediately
-	    console.log(event.target.value);
-	});
+        console.log(event.target.value);
+    });
 
     //global desktop notification settings
-    $("#configButton").on("click", function() {
+    $("#configButton, #configButton1").on("click", function() {
       chatApplication.configMode = true;
       jqchat("#chats").load("/chat/partials/chat.notification.global.html", handleGlobalNotifLayout);
 
@@ -572,6 +557,11 @@ var handleRoomNotifLayout = function() {
       chatApplication.configMode = true;
       enableMessageComposer(false)
       jqchat("#chats").load("/chat/partials/chat.notification.room.html", handleRoomNotifLayout);
+
+      jqchat("#chat-room-detail-avatar, .chat-message.footer, #searchButton2").css("display", "none");
+      jqchat("#userRoomStatus").addClass("hide");
+      jqchat("#chats").css("min-height", window.innerHeight+"px");
+
     });
 
     $(document).on("click", "div.notif-manner div.uiSwitchBtn", function() {
@@ -809,7 +799,15 @@ var handleRoomNotifLayout = function() {
       if (val.charAt(val.length-1)!==' ') val +=" ";
       val += sml + " ";
       $msg.val(val);
-      $msg.focusEnd();
+
+      if (!chatApplication.$mentionEditor) {
+        $msg.focusEnd();
+      } else {
+        var el = $msg.next('div');
+        val = el.html().replace(/<br>/g, '');
+        val = el.html().replace(/&nbsp;/g, ' ');
+        el.html(val + sml + " ");
+      }
       $(".msg-emoticons").parent().removeClass("active");
 
     });
@@ -821,119 +819,12 @@ var handleRoomNotifLayout = function() {
       }
     });
 
-
     $('#chat-search, #chat-search2').keyup(function(event) {
       if (event.keyCode == 27 || event.which == 27) {
         $(this).val('');
       }
       var filter = $(this).val();
       chatApplication.search(filter);
-    });
-
-    $(".create-task-button").on("click", function() {
-      var username = $("#task-add-user").val();
-      var task = $("#task-add-task").val();
-      var dueDate = $("#task-add-date").val();
-      var roomName = chatApplication.targetFullname;
-      var isSpace = false;
-      var roomId = chatApplication.targetUser;
-      var targetUser = chatApplication.targetUser;
-      if (targetUser.indexOf("space-")>-1) {
-        isSpace = true;
-      }
-
-      // Validate empty
-      if (task ===  $("#task-add-task").attr("data-value") || task === "" || dueDate === "") {
-        return;
-      }
-
-      // Validate datetime
-      if (!uiMiniCalendar.isDate(dueDate)) {
-        bootbox.alertError(chatBundleData["exoplatform.chat.date.invalid.message"], function (e) {
-          e.stopPropagation();
-          $("#task-add-date").select();
-        });
-        return;
-      }
-
-      // Get selected users
-      var selectedUsers = "";
-      var selectedFullNames = "";
-      $(".task-user-label").each(function (index) {
-        var name = $(this).attr("data-name");
-        var fullname = $(this).attr("data-fullname");
-        if (index === 0) {
-          selectedUsers = name;
-          selectedFullNames = fullname;
-        }
-        else {
-          selectedUsers += "," + name;
-          selectedFullNames += ", " + fullname;
-        }
-      });
-
-      if (selectedUsers === "") {
-        if (username !== "") {
-          bootbox.alertError(chatBundleData["exoplatform.chat.task.invalidUser.message"].replace("{0}", username), function (e) {
-            e.stopPropagation();
-            $("#task-add-user").select();
-          });
-        }
-        return;
-      }
-      hideMeetingPanel();
-      // Disable button while server updating
-      setActionButtonEnabled('.create-task-button', false);
-
-      // Call server
-      $.ajax({
-        url: chatApplication.jzCreateTask,
-        data: {"username": selectedUsers,
-          "dueDate": dueDate,
-          "task": task,
-          "roomName": roomName,
-          "isSpace": isSpace
-        },
-        success:function(response){
-
-          var options = {
-            type: "type-task",
-            username: selectedUsers,
-            fullname: selectedFullNames,
-            dueDate: dueDate,
-            task: task
-          };
-          var msg = task;
-
-          chatApplication.chatRoom.sendMessage(msg, options, "true");
-          setActionButtonEnabled('.create-task-button', true);
-
-        },
-        error:function (xhr, status, error){
-          console.log("error");
-          setActionButtonEnabled('.create-task-button', true);
-        }
-      });	
-    });
-
-    $('#task-add-user').keyup(function(event) {
-      var prefix = "task";
-      var filter = $(this).val();
-      if (filter.indexOf(',') !== -1) {
-        var inputUserId = $.trim(filter.substring(filter.lastIndexOf(',') + 1));
-        if (inputUserId.length > 0) filter = inputUserId;
-        else return;
-      }
-
-      searchUsers(filter, prefix, event, true, function(name, fullname) {
-        addTaskUserLabel(name, fullname);
-        var pTaskHeight = parseInt($(".meeting-action-task-panel").attr("data-height"));
-        var taskUserListHeight = $(".task-users-list").height();
-        var $popup = $(".meeting-action-popup");
-        $popup.height(pTaskHeight + taskUserListHeight);
-        $popup.css("top", (-Math.abs($popup.height())-4)+"px");
-      });
-
     });
 
     function setMiniCalendarToDateField(dateFieldId) {
@@ -951,7 +842,6 @@ var handleRoomNotifLayout = function() {
         event.cancelBubble = true;
       };
     };
-    setMiniCalendarToDateField('task-add-date');
 
     $(".create-event-button").on("click", function() {
       var space = chatApplication.targetFullname;
@@ -1215,23 +1105,6 @@ var handleRoomNotifLayout = function() {
       $userResults.html("");
     }
 
-    function addTaskUserLabel(name, fullname) {
-      var $usersList = $('.task-users-list');
-      var html = $usersList.html();
-      html += "<span class='uiMention'><a href='javascript:void(0)' class='task-user-label' data-fullname='" + fullname + "' data-name='"+name+"'>"+fullname+"&nbsp;&nbsp;<i class='uiIconClose uiIconLightGray task-user-remove'></i></a></span>";
-      $usersList.html(html);
-      var $taskAddUser = $('#task-add-user');
-      $taskAddUser.val("");
-      $taskAddUser.focus();
-      var $userResults = $(".task-users-results");
-      $userResults.css("display", "none");
-      $userResults.html("");
-
-      $(".task-user-remove").on("click", function() {
-        $(this).parents('.uiMention').remove();
-      });
-    }
-
     function strip(html)
     {
       var tmp = document.createElement("DIV");
@@ -1486,7 +1359,7 @@ var handleRoomNotifLayout = function() {
     $("#room-users-collapse-bar, .participantsList").on("click", function() {
       // toggle room users
       var roomUsersContainer = $(".uiRoomUsersContainerArea");
-      //var roomUsersHeaderTitle = roomUsersContainer.find("#room-users-title");
+      var roomUsersHeaderTitle = roomUsersContainer.find("#room-users-title");
       var collpaseBarIcon = $(this).children("i");
       var allContacts = $("#room-users-button > ul > li:nth-child(1)");
 
@@ -1612,6 +1485,146 @@ function ChatApplication() {
   this.showTeamsHistory = false;
 
   this.showRoomOfflinePeople = false;
+  this.plugins = [];
+}
+
+ChatApplication.prototype.registerEvent = function(plugin) {
+  this.plugins.push(plugin);
+}
+
+ChatApplication.prototype.trigger = function(event, context) {
+  jqchat.each(this.plugins, function(idx, plugin) {
+    if (context.continueSend && plugin[event]) {
+      plugin[event](context);
+    }
+  });
+}
+
+ChatApplication.prototype.initMention = function() {
+  if (!this.$mentionEditor) {
+    var _this = this;
+
+    window.require(["SHARED/jquery", "SHARED/suggester"], function($) {
+      var $msg = $('#msg');
+      $msg.suggester({
+        type : "mix",
+        sourceProviders : ['exo:chat'],
+        showAtCaret: true,
+        renderMenuItem: function(item) {
+          return '<img onerror="this.src=\'/chat/img/Avatar.gif;\'" src="/rest/chat/api/1.0/user/getAvatarURL/'+item.uid+'" width="20px" height="20px"> ' +
+              item.value + ' <span style="float: right" class="chat-status-task chat-status-'+item.status+'"></span>';
+        },
+        renderItem: '<span data-mention="${uid}" class="mention-item" contenteditable="false">${value}<a href="javascript:void(0)" class="remove">×</a></span>',
+      });
+      $msg.suggester('addProvider', 'exo:chat', function(query, callback) {
+        var _this = this;
+        $.ajax({
+          url: chatApplication.jzUsers,
+          data: {"filter": query,
+            "user": chatApplication.username,
+            "token": chatApplication.token,
+            "dbName": chatApplication.dbName
+          },
+          dataType: "json",
+          success: function(data) {
+            var users = [];
+            $.each(data.users, function(idx, user) {
+              users.push({
+                "uid": user.name,
+                "value": user.fullname,
+                "status": user.status
+              });
+            });
+            callback.call(_this, users);
+          }
+        });
+      });
+
+      _this.$mentionEditor = $msg.next('div');
+      var $mentionEditor = _this.$mentionEditor;
+
+      $mentionEditor.focus(function() {
+        chatApplication.updateUnreadMessages();
+      });
+
+      $mentionEditor.on('shown.atwho', function(event, data) {
+          chatApplication.isMentioning = true;
+      });
+      $mentionEditor.on('hidden.atwho', function(event, data) {
+          setTimeout(function() {
+            chatApplication.isMentioning = false;
+          }, 100);
+      });
+
+      $mentionEditor.keyup(function(event) {
+        if (!chatApplication.isMentioning) {
+//          var msg = $('#msg').suggester('getValue');
+          var msg = $mentionEditor.html();
+
+          //    console.log("keyup : "+event.which + ";"+msg.length+";"+keydown);
+          if ( event.which === 13 && msg.trim().length>=1 && !(event.shiftKey||event.ctrlKey||event.altKey)) {
+            //console.log("sendMsg=>"+username + " : " + room + " : "+msg);
+            if ( !msg || event.keyCode == 13 && (event.shiftKey||event.ctrlKey||event.altKey) ) {
+              return false;
+            }
+
+            msg = msg.replace(/<br>/g, '\n');
+
+            $div = $('#msgtemp');
+            if ($div.length == 0) {
+              $div = $('<div id="msgtemp" style="display: none;"></div>');
+              $('body').append($div);
+            }
+            $div.html(msg);
+
+            $mention = $div.find('span[class="mention-item"]');
+            $mention.each(function( index ) {
+              $( this ).replaceWith('@' + $( this ).attr('data-mention') + ' ');
+            });
+
+            msg = $div.text();
+
+            chatApplication.sendMessage(msg);
+            $('#msg').suggester('clearValue');
+          }
+          // UP Arrow
+          if (event.which === 38 && msg.length === 0) {
+            var $uimsg = chatApplication.chatRoom.getUserLastMessage();
+            var $uimsgdata = $uimsg.find(".msg-data");
+            if ($uimsgdata.length === 1) {
+              chatApplication.openEditMessagePopup($uimsgdata.attr("data-id"), $uimsgdata.html());
+            }
+          }
+
+          if ( chatApplication.keydown === 18 ) {
+            chatApplication.keydown = -1;
+          }
+        } else {
+          if ($mentionEditor.next('ul').css('display') == 'none') {
+            chatApplication.isMentioning = false;
+          }
+        }
+      });
+
+      jqchat('#msg').on('focus', function() {
+        $mentionEditor.focus();
+        $mentionEditor.html($mentionEditor.html().replace(/<br>/g, ''));
+
+        var range = document.createRange();
+        var sel = window.getSelection();
+        if (!$mentionEditor[0].childNodes.length) {
+          range.setStart($mentionEditor[0], $mentionEditor[0].childNodes.length);
+        } else {
+          range.setStart($mentionEditor[0].childNodes[0], $mentionEditor[0].childNodes.length);
+        }
+        range.collapse(true);
+        sel.removeAllRanges();
+        sel.addRange(range);
+      });
+    });
+  }
+
+
 }
 
 /**
@@ -1875,7 +1888,7 @@ ChatApplication.prototype.resize = function() {
       jqchat("#room-users-collapse-bar").css("line-height", (heightChat - 44) + "px");
 
   }else{
-      jqchat("#chats").height(heightChat -53);
+      jqchat("#chats").height(heightChat - 38);
       jqchat("#chat-users").height(heightChat -44);
 //      jqchat("#room-users-list").height(heightChat - 44 -29); // remove header and padding
       jqchat("#room-users-list").height(heightChat); // remove header and padding
@@ -1946,7 +1959,6 @@ ChatApplication.prototype.initChatPreferences = function() {
   this.showTeams = true;
   if (jzGetParam("chatShowTeams"+this.username) === "false") this.showTeams = false;
 };
-
 
 /**
  * Init Chat Profile
@@ -2450,7 +2462,6 @@ ChatApplication.prototype.reloadCurrentItem = function(room) {
     var spaceFullName = jqchat("<div/>").html(room.escapedFullname).text();
     if (chatApplication.targetFullname !== spaceFullName) {
       jqchat('.target-user-fullname').text(spaceFullName);
-      //console.log("here1");
       chatApplication.targetUser = room.user;
       chatApplication.targetFullname = spaceFullName;
       chatApplication.loadRoom();
@@ -2560,7 +2571,6 @@ ChatApplication.prototype.loadRoom = function() {
     jqchat("#chat-team-button").css("display", "none");
     this.targetFullname = jqchat("<div/>").html(this.targetFullname).text();
     jqchat(".target-user-fullname").text(this.targetFullname);
-  //  console.log("here2");
 
     if(navigator.platform.indexOf("Linux") === -1 || jqchat.browser.chrome) {
       jqchat(".btn-weemo-conf").css("display", "none");
@@ -3131,6 +3141,13 @@ jqchat('#back').on("click", function() {
   jqchat('#chat-users .users-online > td:nth-child(1),#chat-users .users-online > td:nth-child(2)').on("click", function() {
     if(window.innerWidth <= 767){
 
+        jqchat("#chat-application .uiGrayLightBox .uiSearchInput").removeClass("displayContent");
+       /* $("#chat-application .uiSearchInput.displayContent #chat-search").attr("value", "");*/
+        jqchat('input#chat-search.input-with-value.span4').val('');
+        var filter = jqchat('input#chat-search.input-with-value.span4').val();
+        chatApplication.search(filter);
+
+
         var $chatStatusPanel = jqchat(".chat-status-panel");
 
         $chatStatusPanel.css("display", "none");
@@ -3318,9 +3335,18 @@ ChatApplication.prototype.setStatusInvisible = function() {
  */
 ChatApplication.prototype.sendMessage = function(msg, callback) {
 
+  var options = {};
+  var context = {"msg": msg, "options": options, "callback": callback, "continueSend": true};
+
+  this.trigger("beforeSend", context);
+  if (!context.continueSend) {
+    return;
+  }
+  msg = context.msg;
+  options = context.options;
+  callback = context.callback;
 
   var isSystemMessage = (msg.indexOf("/")===0 && msg.length>2) ;
-  var options = {};
   var sendMessageToServer = true;
   if (isSystemMessage) {
     sendMessageToServer = false;
@@ -3356,7 +3382,6 @@ ChatApplication.prototype.sendMessage = function(msg, callback) {
   if (sendMessageToServer) {
     this.chatRoom.sendMessage(msg, options, isSystemMessage, callback);
   }
-
 };
 
 
