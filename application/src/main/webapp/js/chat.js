124d4547bbb4ed4daf72e5a6dee97d8b2abc768e
boubaker <bkhanfir@exoplatform.com>
2016-12-21 12:39:25 +0100

    CHAT-570 getStatus is called very frequently (#87)

--- application/src/main/webapp/js/chat.js
+++ application/src/main/webapp/js/chat.js
@@ -3059,7 +3059,6 @@ jqchat('#back').on("click", function() {
     }, 500);
     jqchat("#chat-video-button").attr("style", "");
 });
-var roomUserInterval = -1;
   jqchat('#chat-users .users-online > td:nth-child(1),#chat-users .users-online > td:nth-child(2)').on("click", function() {
     if(window.innerWidth <= 767){
 
@@ -3101,8 +3100,6 @@ var roomUserInterval = -1;
       jqchat(".left-chat").css("display", "none");
       jqchat(".room-name").html(thiss.targetFullname);
     }
-    clearInterval(roomUserInterval);
-    roomUserInterval = setInterval(function(){ chatNotification.getStatus(thiss.targetUser, userRoomStatus); }, 2500);
   });
 
   jqchat('#chat-users .users-online').on("mouseenter", function() {
