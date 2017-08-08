ccff40828766c60c73f8b9139dd46d26fedff8b5
Tran The Trong <trongtt@exoplatform.com>
2017-08-08 19:04:09 +0700

    CHAT-712 Save notes, Edit, Delete and Quote links appear when mouse over a message which contains a file

--- application/src/main/webapp/js/chat-modules.js
+++ application/src/main/webapp/js/chat-modules.js
@@ -825,7 +825,7 @@ ChatRoom.prototype.generateMessageHTML = function(message) {
   }
   msRightInfo +=            this.getMessageInfo(message);
   msRightInfo += "        </div>";
-  if (message.type !== "DELETED") {
+  if (message.type !== "DELETED" && message.isSystem !== true) {
     msRightInfo += "      <div class='msAction msg-actions'><span style='display: none;' class='msg-data' data-id='"+message.msgId+"' data-fn='"+message.fullname+"' data-timestamp='" + message.timestamp + "'>"+message.msg+"</span>";
     msRightInfo += "        <a href='#' class='msg-action-savenotes'>" + chatBundleData["exoplatform.chat.notes"] + "</a> |";
     if (message.user === this.username) {
