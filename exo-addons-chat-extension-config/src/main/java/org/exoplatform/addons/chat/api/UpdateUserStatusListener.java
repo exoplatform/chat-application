f7f973036d2a485c794555cd3efc01ccd0b6bd03
sayachi <sayachi@exoplatform.com>
2017-11-24 11:04:06 +0100

    CHAT-755 User chat status is not updated after session timeout (#156)
    CHAT-755 User chat status is not updated after session timeout

---
+++ exo-addons-chat-extension-config/src/main/java/org/exoplatform/addons/chat/api/UpdateUserStatusListener.java
@@ -0,0 +1,39 @@
+package org.exoplatform.addons.chat.api;
+
+import org.exoplatform.commons.utils.CommonsUtils;
+import org.exoplatform.services.listener.Event;
+import org.exoplatform.services.listener.Listener;
+import org.exoplatform.services.security.ConversationRegistry;
+import org.exoplatform.services.security.ConversationState;
+import org.exoplatform.services.security.Identity;
+import org.exoplatform.services.user.UserStateModel;
+import org.exoplatform.services.user.UserStateService;
+
+import java.util.Calendar;
+
+
+/**
+ * This listener will be used to update chat status of current user in the case session destroyed.
+ */
+public class UpdateUserStatusListener extends Listener<ConversationRegistry, ConversationState> {
+    public static final String STATUS_OFFLINE = "offline";
+
+    @Override
+    public void onEvent(Event<ConversationRegistry, ConversationState> event) throws Exception {
+        UserStateService userStateService = CommonsUtils.getService(UserStateService.class);
+        ConversationState data = event.getData();
+        if (data != null) {
+            Identity identity = data.getIdentity();
+            if (identity != null) {
+                String userId = identity.getUserId();
+                UserStateModel userStateModel = userStateService.getUserState(userId);
+                if (userStateModel == null) return;
+                userStateModel.setLastActivity(Calendar.getInstance().getTimeInMillis() - userStateService.getDelay());
+                userStateModel.setStatus(STATUS_OFFLINE);
+                userStateService.save(userStateModel);
+            }
+        }
+
+    }
+}
+
