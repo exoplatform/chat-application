78b2d3de9e2f4ccc40a4949c0d0d395f6cb74daa
alaameddeb <aemeddeb@exoplatform.com>
2018-08-06 13:20:03 +0200

    CHAT-947 Only first URL in message is clickable (#230)

--- application/src/main/webapp/vue-app/messageFilter.js
+++ application/src/main/webapp/vue-app/messageFilter.js
@@ -3,31 +3,38 @@ export default function(msg, highlight, emojis) {
     return msg;
   }
   let message = '';
-  const words = msg.split(' ');
-  
-  words.forEach(w => {
-    // check link
-    if (w.indexOf('http:') === 0 || w.indexOf('https:') === 0 || w.indexOf('ftp:') === 0) {
-      // check image link
-      if (w.endsWith('.jpg') || w.endsWith('.png') || w.endsWith('.gif') || w.endsWith('.JPG') || w.endsWith('.PNG') || w.endsWith('.GIF')) {
-        w = `<a href="${w}" target='_blank'><img src="${w}" alt="${w}"/></a>`;
-      } else {
-        w = `<a href="${w}" target='_blank'>${w}</a>`;
-      }
-    } else if (highlight !== '' && w.indexOf(highlight) >= 0) {
-      w = w.replace(highlight, `<span class="search-highlight">${highlight}</span>`);
-    } else {
-      // check emoticons
-      const wordCaseSensitive = w.replace(/\.|\?|!/, '');
-      const wordCaseInsensitive = wordCaseSensitive.toLowerCase();
-      const emoticon = emojis.find(emoticon => emoticon.keys.indexOf(wordCaseInsensitive) >= 0);
-      if(emoticon) {
-        w = w.replace(wordCaseSensitive, `<span class="chat-emoticon ${emoticon.class}"><span>${wordCaseSensitive}</span></span>`);
-      }
-    }
-    message += `${w} `;
-  });
+  const lines = msg.split(/<br\s*\/?>/);
 
+  lines.forEach( (line, index) => {
+    const words = line.split(' ');
+    words.forEach(w => {
+      // check link
+      if (w.indexOf('http:') === 0 || w.indexOf('https:') === 0 || w.indexOf('ftp:') === 0) {
+        // check image link
+        if (w.endsWith('.jpg') || w.endsWith('.png') || w.endsWith('.gif') || w.endsWith('.JPG') || w.endsWith('.PNG') || w.endsWith('.GIF')) {
+          w = `<a href="${w}" target='_blank'><img src="${w}" alt="${w}"/></a>`;
+        } else {
+          w = `<a href="${w}" target='_blank'>${w}</a>`;
+        }
+      } else if (highlight !== '' && w.indexOf(highlight) >= 0) {
+        w = w.replace(highlight, `<span class="search-highlight">${highlight}</span>`);
+      } else {
+        // check emoticons
+        const wordCaseSensitive = w.replace(/\.|\?|!/, '');
+        const wordCaseInsensitive = wordCaseSensitive.toLowerCase();
+        const emoticon = emojis.find(emoticon => emoticon.keys.indexOf(wordCaseInsensitive) >= 0);
+        if(emoticon) {
+          w = w.replace(wordCaseSensitive, `<span class="chat-emoticon ${emoticon.class}"><span>${wordCaseSensitive}</span></span>`);
+        }
+      }
+      message += `${w} `;
+    });
+
+    if (index !== lines.length - 1) { // avoid adding break line at last line
+      message += '<br>';
+    }
+  });
+  
   //check quote
   if (message.indexOf('[quote=') > -1) {
     message = checkQuotes(message);
