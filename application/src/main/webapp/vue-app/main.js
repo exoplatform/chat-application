76f620383156f6d524dd9881bca4ec340e99a84c
Vincent Sellier <vsellier@exoplatform.com>
2018-08-30 12:41:20 +0200

    Revert "CHAT-951 Error when multiple instances of VueI18n in page (#240)"
    This reverts commit 1c18e35025191d78c277d5865dc5e63a990ce72b.

--- application/src/main/webapp/vue-app/main.js
+++ application/src/main/webapp/vue-app/main.js
@@ -1,4 +1,5 @@
 import Vue from 'vue';
+import exoi18n from '../js/lib/exo-i18n';
 import {addCaretJQueryExtension} from '../js/lib/text-caret';
 import ExoChatApp from './components/ExoChatApp.vue';
 import ExoMiniChatApp from './components/ExoMiniChatApp.vue';
@@ -6,8 +7,7 @@ import {chatConstants} from './chatConstants.js';
 
 import './../css/main.less';
 
-const lang = typeof eXo !== 'undefined' ? eXo.env.portal.language : 'en';
-const url = `${chatConstants.PORTAL}/${chatConstants.PORTAL_REST}/i18n/bundle/locale.portlet.chat.Resource-${lang}.json`;
+const lang = typeof eXo !== 'undefined' ? eXo.env.portal.language : '';
 
 Vue.prototype.$constants = chatConstants;
 
@@ -32,7 +32,7 @@ Vue.directive('hold-tap', function (el, binding, vnode) {
   }
 });
 
-exoi18n.loadLanguageAsync(lang, url).then(i18n => {
+exoi18n.loadLanguageAsync(lang).then(i18n => {
   if ($('#chatApplication').length) {
     new Vue({
       el: '#chatApplication',
