ae5506be2848d214ab3212dbb0e69945a9870c8f
Boubaker Khanfir <bkhanfir@exoplatform.com>
2019-10-07 10:18:41 +0100

    CHAT-1026 Fix slowness in chat application (#297)

---
+++ services/src/main/java/org/exoplatform/addons/chat/listener/UserLoginListener.java
@@ -0,0 +1,25 @@
+package org.exoplatform.addons.chat.listener;
+
+import org.apache.commons.lang3.StringUtils;
+
+import org.exoplatform.services.listener.*;
+import org.exoplatform.services.security.*;
+
+/**
+ * This listener will be used to update current user chat rooms list on login.
+ */
+@Asynchronous
+public class UserLoginListener extends Listener<ConversationRegistry, ConversationState> {
+  @Override
+  public void onEvent(Event<ConversationRegistry, ConversationState> event) throws Exception {
+    ConversationState conversationState = ConversationState.getCurrent();
+
+    String userId = conversationState == null
+        || conversationState.getIdentity() == null ? null : conversationState.getIdentity().getUserId();
+
+    if (StringUtils.isBlank(userId) || StringUtils.equalsIgnoreCase(userId, IdentityConstants.ANONIM)) {
+      return;
+    }
+    ServerBootstrap.saveSpaces(userId, ServerBootstrap.getDBName());
+  }
+}
