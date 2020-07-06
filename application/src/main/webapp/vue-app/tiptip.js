a05bb229d764eabbb474cffb46b9aaa0e31675de
Sara Boutej <40363501+SaraBoutej@users.noreply.github.com>
2020-07-06 15:18:06 +0100

    Remove chat icon from user popover (#339)

--- application/src/main/webapp/vue-app/tiptip.js
+++ application/src/main/webapp/vue-app/tiptip.js
@@ -5,20 +5,6 @@ export function initTiptip() {
   eXo.social = eXo.social ? eXo.social : {};
   eXo.social.tiptip = eXo.social.tiptip ? eXo.social.tiptip : {};
   eXo.social.tiptip.extraActions = eXo.social.tiptip.extraActions ? eXo.social.tiptip.extraActions : [];
-  eXo.social.tiptip.extraActions.push({
-    appendContentTo(divUIAction, ownerId, type) {
-      if(!type) {
-        type = 'username';
-      }
-      divUIAction.append(`<a title="Chat"  \
-        class="btn chatPopupOverlay chatPopup" \
-        type="button" \
-        onclick="document.dispatchEvent(new CustomEvent('${chatConstants.ACTION_ROOM_OPEN_CHAT}', {detail: {name: '${ownerId}', type: '${type}'}}))"> \
-          <i class="uiIconBannerChat uiIconLightGray"></i> \
-          Chat \
-      </a>`);
-    }
-  });
   if ($('.spaceMenuNav .spaceMenuNavHeader .spaceMenuApps').length && eXo && eXo.env && eXo.env.portal && eXo.env.portal.spaceGroup) {
     $('.spaceMenuNav .spaceMenuNavHeader .spaceMenuApps').append(`<li>
         <a onclick="document.dispatchEvent(new CustomEvent('${chatConstants.ACTION_ROOM_OPEN_CHAT}', {detail: {name: '${eXo.env.portal.spaceGroup}', type: 'space-name'}}))" \
