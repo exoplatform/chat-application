1afaf508842978cbee4e0368d89bc9e3b6200c8f
Moez Diha <mdiha@exoplatform.com>
2021-01-28 12:06:33 +0100

    MAINT-37615: Remove unneeded dispatch event.

--- application/src/main/webapp/vue-app/components/ExoChatApp.vue
+++ application/src/main/webapp/vue-app/components/ExoChatApp.vue
@@ -116,8 +116,6 @@ export default {
     document.addEventListener(chatConstants.EVENT_GLOBAL_UNREAD_COUNT_UPDATED, this.totalUnreadMessagesUpdated);
     document.addEventListener(chatConstants.ACTION_ROOM_SHOW_PARTICIPANTS, () => this.participantsArea = true);
     document.addEventListener(chatConstants.ACTION_ROOM_OPEN_CHAT, this.openRoom);
-
-    document.dispatchEvent(new CustomEvent('chat-external-updated'));
   },
   destroyed() {
     document.removeEventListener(chatConstants.EVENT_DISCONNECTED, this.changeUserStatusToOffline);
