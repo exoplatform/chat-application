c79824161b3921d8d990f423f4ff6bf95185da1b
GouadriaHanen <49310007+GouadriaHanen@users.noreply.github.com>
2021-03-05 14:03:35 +0100

    TASK-37051: Refix redirect URL after clicking on the space logo (#242)
    * MAINT-37615: Allow the space manager to enable or disable the chat of a space. (#186)
    
    I added a property (isEnabled) to chat rooms and i added two methods setRoomEnabled and isRoomEnabled to update this property and retrieve its value.
    I made sure that space's rooms enabled property is set at server start-up if not already initialized.
    
    * Fix-MAINT-37615: Make sure to dispatch event chat-external-updated after creation of chat's Vue instance. (#195)
    
    At space setting's instance Vue creation the chat Vue instance is not already created, i made sure to dispatch the event of chat-external-updated after the creation of chat's Vue instance.
    
    * MAINT-37615: Make sure to retrieve only enabled chat rooms. (#211)
    
    * TASK-37051: Refix redirect URL after clicking on the space logo
    
    * removed unused instructions
    
    * removed getspace method
    
    * removed changes
    
    * updates
    
    Co-authored-by: Moez Diha <38471028+mdiha@users.noreply.github.com>

--- application/src/main/webapp/vue-app/components/ExoChatContact.vue
+++ application/src/main/webapp/vue-app/components/ExoChatContact.vue
@@ -118,7 +118,7 @@ export default {
         invisible: this.$t('exoplatform.chat.invisible'),
         offline: this.$t('exoplatform.chat.button.offline')
       },
-      spaceContact: {},
+      spaceGroupUri : '',
     };
   },
   computed: {
@@ -162,15 +162,11 @@ export default {
     isActive() {
       return this.type === 'u' && !this.isEnabled ? 'inactive' : 'active';
     },
-    spaceGroupUri() {
-      return this.spaceContact && this.spaceContact.groupId && this.spaceContact.groupId.replace(/\//g, ':');
-    },
     contactUrl() {
       if (this.type === 'u') {
         return getUserProfileLink(this.userName);
       } else if (this.type === 's') {
-        const spaceId = this.name.toLowerCase().split(' ').join('_');
-        return `${eXo.env.portal.context}/g/${this.spaceGroupUri}/${spaceId}`;
+        return this.getSpaceURI() ;
       }
       return '#';
     }
@@ -179,9 +175,6 @@ export default {
     document.addEventListener(chatConstants.EVENT_DISCONNECTED, this.setOffline);
     document.addEventListener(chatConstants.EVENT_CONNECTED, this.setOnline);
     document.addEventListener(chatConstants.EVENT_RECONNECTED, this.setOnline);
-    if (this.type === 's') {
-      this.getSpace();
-    }
   },
   destroyed() {
     document.removeEventListener(chatConstants.EVENT_DISCONNECTED, this.setOffline);
@@ -198,12 +191,14 @@ export default {
     setOffline() {
       this.isOnline = false;
     },
-    getSpace() {
-      return getSpaceByPrettyName(this.name).then((space) => {
+    getSpaceURI() {
+      const spaceId = this.name.toLowerCase().split(' ').join('_');
+      getSpaceByPrettyName(this.name).then((space) => {
         if (space && space.identity) {
-          this.spaceContact = space;
+          this.spaceGroupUri= space.groupId.replace(/\//g, ':');
         }
       });
+      return `${eXo.env.portal.context}/g/${this.spaceGroupUri}/${spaceId}`;
     }
   }
 };
