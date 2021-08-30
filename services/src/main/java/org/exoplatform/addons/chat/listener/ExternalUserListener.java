dae030fa1275dd2444bf99d93a2c9875076ae4e3
GouadriaHanen <hgouadria@exoplatform.com>
2021-08-30 17:23:25 +0100

    TASK-40995:External user flag is still displayed next to the user fullname
    when adding a user to 'External group ' an external flag must be displayed next to his name in chat room and chat drawer  :the External Flag isn't always displayed because in the actual behavior  setting user property is set when the user log in  and not updated when removed from ,fixed by adding a listener from MembershipEventListener to update the user enabled property one added or removed from 'External group'

---
+++ services/src/main/java/org/exoplatform/addons/chat/listener/ExternalUserListener.java
@@ -0,0 +1,42 @@
+package org.exoplatform.addons.chat.listener;
+
+import org.exoplatform.chat.services.UserService;
+import org.exoplatform.commons.utils.CommonsUtils;
+import org.exoplatform.container.PortalContainer;
+import org.exoplatform.services.listener.Asynchronous;
+import org.exoplatform.services.log.ExoLogger;
+import org.exoplatform.services.log.Log;
+import org.exoplatform.services.organization.Membership;
+import org.exoplatform.services.organization.MembershipEventListener;
+
+public class ExternalUserListener extends MembershipEventListener {
+
+  private static final Log LOG = ExoLogger.getLogger(ExternalUserListener.class);
+  private static final String PLATFORM_EXTERNALS_GROUP  = "/platform/externals";
+
+  private UserService userService;
+
+
+  @Override
+  public void postSave(Membership m, boolean isNew) {
+    if (m.getGroupId().equals(PLATFORM_EXTERNALS_GROUP)) {
+      try {
+        ServerBootstrap.setExternalUser(m.getUserName(),"true");
+      } catch (Exception e) {
+        LOG.error("Error while saving the external property for user  {}", m.getUserName(), e);
+      }
+    }
+  }
+
+  @Override
+  public void postDelete(Membership m) {
+    if (m.getGroupId().equals(PLATFORM_EXTERNALS_GROUP)) {
+      try {
+        ServerBootstrap.setExternalUser(m.getUserName(),"false");
+      } catch (Exception e) {
+        LOG.error("Error while saving the external property for user  {}", m.getUserName(), e);
+      }
+    }
+  }
+
+}
