e01710a0858f36065f842854eff190c609b10e65
Ahmed Ben Mbarek <abenmbarek@exoplatform.com>
2021-11-23 15:00:35 +0100

    MAINT-28221 : fix the status list box covers the bar

--- application/webpack.dev.js
+++ application/webpack.dev.js
@@ -3,8 +3,7 @@ const merge = require('webpack-merge');
 const webpackCommonConfig = require('./webpack.common.js');
 
 // change the server path to your server location path
-const exoServerPath = "/exo-server/";
-
+const exoServerPath = "/home/exo/Server/binaries/plfent-6.3.x-maintenance-20211122.164455-844/platform-6.3.x-maintenance-SNAPSHOT/";
 let config = merge(webpackCommonConfig, {
   output: {
     path: path.resolve(__dirname, exoServerPath + 'webapps/chat/')
