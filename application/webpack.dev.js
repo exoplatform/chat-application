d75760263e4a25e36eaa32185796e875f76d1960
Ahmed Ben Mbarek <abenmbarek@exoplatform.com>
2021-11-24 14:50:58 +0100

    PR review : revert changes webpack.dev.js

--- application/webpack.dev.js
+++ application/webpack.dev.js
@@ -4,6 +4,7 @@ const webpackCommonConfig = require('./webpack.common.js');
 
 // change the server path to your server location path
 const exoServerPath = "/exo-server/";
+
 let config = merge(webpackCommonConfig, {
   output: {
     path: path.resolve(__dirname, exoServerPath + 'webapps/chat/')
