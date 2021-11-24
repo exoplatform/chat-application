050bc5c6982b4188498735092fbe5f0affb78dab
Ahmed Ben Mbarek <abenmbarek@exoplatform.com>
2021-11-24 13:51:57 +0100

    MAINT-28221 : reset the server path after fixing the list box status

--- application/webpack.dev.js
+++ application/webpack.dev.js
@@ -3,7 +3,7 @@ const merge = require('webpack-merge');
 const webpackCommonConfig = require('./webpack.common.js');
 
 // change the server path to your server location path
-const exoServerPath = "/home/exo/Server/binaries/plfent-6.3.x-maintenance-20211122.164455-844/platform-6.3.x-maintenance-SNAPSHOT/";
+const exoServerPath = "/exo-server/";
 let config = merge(webpackCommonConfig, {
   output: {
     path: path.resolve(__dirname, exoServerPath + 'webapps/chat/')
