d19c3e52c496a16ed7046b3e44a7ce7ffdc72e83
sofyenne <shajchedli@exoplatform.com>
2022-11-28 11:46:02 +0100

    fix code sonar

--- application/src/main/webapp/vue-app/components/modal/ExoChatDrawer.vue
+++ application/src/main/webapp/vue-app/components/modal/ExoChatDrawer.vue
@@ -1,3 +1,16 @@
+<!--
+Copyright (C) 2022 eXo Platform SAS.
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+You should have received a copy of the GNU Affero General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+-->
 <template>
   <div class="VuetifyApp">
     <v-app
@@ -136,11 +149,10 @@
 <script>
 import * as chatServices from '../../chatServices';
 import * as chatWebStorage from '../../chatWebStorage';
-import {installExtensions} from '../../extension';
 import {chatConstants} from '../../chatConstants';
 import * as chatWebSocket from '../../chatWebSocket';
 import * as desktopNotification from '../../desktopNotification';
-import {miniChatTitleActionComponents,composerApplications} from '../../extension';
+import {miniChatTitleActionComponents,composerApplications,installExtensions} from '../../extension';
 import {getSpaceAvatar, getUserAvatar} from '../../chatServices';
 
 export default {
