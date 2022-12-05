7757bf43ca1ddf6a9cbd7257091f05cc3e81c64e
sofien haj chedli <shajchedli@exoplatform.com>
2022-12-05 14:00:12 +0100

    feat: Implement quick create chat discussion drawer actions - EXO-60971  (#571)
    We will implement save and cancel actions of chat quick create discussion drawer

--- application/src/main/webapp/vue-app/components/ExoChatContactList.vue
+++ application/src/main/webapp/vue-app/components/ExoChatContactList.vue
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2022 eXo Platform SAS.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License
+ * as published by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <gnu.org/licenses>.
+ */
 <template>
   <div class="contactListContainer" role="navigation">
     <div v-if="ap">
@@ -384,6 +400,7 @@ export default {
     );
     this.contactList = this.contacts.slice();
     this.contactsToDisplay = this.contacts.slice();
+    this.$root.$on('quick-chat-discussion-saved', e => this.roomSaved(e));
   },
   destroyed() {
     document.removeEventListener(chatConstants.EVENT_ROOM_MEMBER_LEFT, this.leftRoom);
--- application/src/main/webapp/vue-app/components/modal/ExoChatQuickCreateDiscussionDrawer.vue
+++ application/src/main/webapp/vue-app/components/modal/ExoChatQuickCreateDiscussionDrawer.vue
@@ -24,7 +24,7 @@
       <span class="PopupTitle"> <v-icon left @click="close">mdi-arrow-left</v-icon>{{ $t('exoplatform.chat.quick.create.discussion') }}</span>
     </template>
     <template slot="content">
-      <v-form ref="Quicksuggester" class="pa-2 ms-2 mt-4">
+      <v-form ref="Quicksuggester" id="Quicksuggester" class="pa-2 ms-2 mt-4">
         <div class="d-flex flex-column flex-grow-1">
           <div class="d-flex flex-column mb-2">
             <label class="d-flex flex-row font-weight-bold my-2">{{ $t('exoplatform.chat.quick.create.discussion.add.people') }}</label>
@@ -62,6 +62,17 @@
         </div>
       </v-form>
     </template>
+    <template slot="footer">
+      <div class="d-flex my-2 flex-row justify-end">
+        <button class="mx-5 px-8 btn" @click="close()">{{ $t('exoplatform.chat.cancel') }}</button>
+        <button
+          :disabled="disabledSaveButton"
+          class="btn btn-primary"
+          @click="quickCreateChatDiscussion">
+          {{ $t('exoplatform.chat.quick.discussion.add') }}
+        </button>
+      </div>
+    </template>
   </exo-drawer>
 </template>
 
@@ -69,6 +80,8 @@
 <script>
 
 import {chatConstants} from '../../chatConstants';
+import * as chatServices from '../../chatServices';
+
 export default {
   name: 'ExoChatDrawer',
   components: {},
@@ -92,6 +105,10 @@ export default {
     displayQuickDiscussionInputName() {
       return this.participantItem !== null && this.participantItem.length > 1 ? true : false;
     },
+    disabledSaveButton(){
+      return !(this.participantItem && this.participantItem.length === 1 && !this.validNewRoomName || 
+        this.participantItem && this.participantItem.length > 1 && this.validNewRoomName);
+    }
   },
   watch: {
     participants() {
@@ -137,6 +154,53 @@ export default {
         this.participantItem.splice(index, 1);
       }
     },
+    openRoomQuickDiscussion() {
+      if (this.fullName) {
+        let users = this.participantItem.map(user => user.identity.remoteId || user.identity.name);
+        if (users.indexOf(eXo.chat.userSettings.username) < 0) {
+          users.unshift(eXo.chat.userSettings.username);
+        }
+        users = users.join(',');
+        chatServices.saveRoom(eXo.chat.userSettings, this.fullName, users, null)
+          .then(resp => {
+            const HTTP_OK_CODE = 200;
+            if (resp.status === HTTP_OK_CODE) {
+              return resp.json();
+            } else {
+              return resp.text();
+            }
+          })
+          .then((response) => {
+            if (response && response.room) {
+              this.$root.$emit('quick-chat-discussion-saved', response.room);
+              this.close();
+              this.displayAlert(this.$t('exoplatform.chat.quick.create.discussion.success.notification'));
+            } else if (response === 'roomAlreadyExists.creator') {
+              this.displayAlert(this.$t('exoplatform.chat.CreatorErrorRoomCreationMessage'),'error');
+            } else if (response === 'roomAlreadyExists.notCreator') {
+              this.displayAlert(this.$t('exoplatform.chat.NotCreatorErrorRoomCreationMessage'),'error');
+            }
+          });
+      }
+    },
+    displayAlert(message, type) {
+      document.dispatchEvent(new CustomEvent('notification-alert', {detail: {
+        message,
+        type: type || 'success',
+      }}));
+    },
+    openOneToOneQuickDiscussion(){
+      const remoteId = this.participantItem[0].identity.remoteId;
+      this.close();
+      document.dispatchEvent(new CustomEvent(chatConstants.ACTION_ROOM_OPEN_CHAT, {detail: {name: remoteId, type: 'username'}}));
+    },
+    quickCreateChatDiscussion(){
+      if (this.validNewRoomName){
+        this.openRoomQuickDiscussion();
+      } else {
+        this.openOneToOneQuickDiscussion();
+      }
+    }
   }
 };
 </script>
